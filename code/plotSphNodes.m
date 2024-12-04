5b26a3f09931f66e3d4e4c6639db21ec0df23668
gradywright <gradywright@boisestate.edu>
2024-12-04 13:48:32 -0700

    Allow markers to be changed.

--- code/plotSphNodes.m
+++ code/plotSphNodes.m
@@ -1,4 +1,4 @@
-function [varargout] = plotSphNodes(x,mrkrsize)
+function [varargout] = plotSphNodes(x,mrkrsize,mrkspec)
 %PLOTSPHNODES Makes a simple plot of a given set of nodes on the surface of
 %   the unit sphere.
 %
@@ -16,8 +16,11 @@ function [varargout] = plotSphNodes(x,mrkrsize)
 
 % Author: Grady Wright, 2014
 
-if ( nargin < 2 )
-    mrkrsize = 6;
+if ( nargin < 3 )
+    mrkspec = 'b.';
+    if ( nargin < 2 )
+        mrkrsize = 6;
+    end
 end
 
 % Make sure the nodes are on the unit sphere.
@@ -29,6 +32,7 @@ x = bsxfun(@rdivide,x,sqrt(sum(x.^2,2)));
 [xx,yy,zz] = sphere(101);
 % Color of the sphere will be yellow:
 clr = [255 255 102]/255;
+clr = [1 1 1]*0.90;
 % Plot the sphere
 cax = newplot;
 hold on;
@@ -37,7 +41,7 @@ surf(xx,yy,zz,1+0*xx,'EdgeColor','None','FaceColor',clr);
 %
 % Add the nodes as black solid circles.
 %
-plot3(x(:,1),x(:,2),x(:,3),'k.','MarkerSize',mrkrsize);
+plot3(x(:,1),x(:,2),x(:,3),mrkspec,'MarkerSize',mrkrsize);
 hold off;
 daspect([1 1 1]);
 view(3);
